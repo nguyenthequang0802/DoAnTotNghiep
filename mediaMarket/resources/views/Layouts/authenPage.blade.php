29859703cdb98f32f3cdf2f412cf15a238a6a425
nguyenthequang0802 <quangnguyenthe7@gmail.com>
2024-05-16 22:47:28 +0700

    upload front-end adminPage

--- mediaMarket/resources/views/Layouts/authenPage.blade.php
+++ mediaMarket/resources/views/Layouts/authenPage.blade.php
@@ -53,7 +53,6 @@
                                             <a href="{{ route('admin.register.form') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">{{ __('Register') }}</a>
                                         </li>
                                 @endif
-
                             </ul>
                         @else
                             <div class="px-4 py-3">
